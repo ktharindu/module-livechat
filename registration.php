087a77ecbbcf41677a05519bd8eb51805c5a346a
Kusal Fernando <ktharindu@gmail.com>
2019-11-17 15:47:32 +0530

    first commit

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Agt Coding Challenge.
+ *
+ * @category    Agt
+ * @package     Agt_LiveChat
+ * @author      Kusal Fernando <kusal@gmail.com>
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Agt_LiveChat',
+    __DIR__
+);
